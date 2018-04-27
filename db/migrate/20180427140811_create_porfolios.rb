a5bef88cf12158bed784390c1ebbd1e4467a20d7
Gabe Ochoa <gochoa5190@gmail.com>
2018-04-27 10:19:14 -0400

    Added portfolio items via resource generator

---
+++ db/migrate/20180427140811_create_porfolios.rb
@@ -0,0 +1,16 @@
+class CreatePorfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :porfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
+
+
+
