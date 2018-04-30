721ae148fa4303dfb8efd30daef78c57283a1da4
Gabe Ochoa <gochoa5190@gmail.com>
2018-04-30 10:16:19 -0400

    creating custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topics|
+  Topic.create(
+    title: "Topic #{topics}"
+    )
+end
+
+puts 'Three topics have been created'
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog post number #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    topic_id: Topic.last.id
     )
 end
 
@@ -17,14 +26,24 @@ end
 puts "5 skills have been created!"
 
 
-12.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image:"http://via.placeholder.com/350x200"
     )
 end
 
-puts "12 portfolio items created"
+1.times do |portfolio_item|
+Portfolio.create!(
+  title: "Portfolio title: #{portfolio_item}",
+  subtitle: "Angular",
+  body: "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt",
+  main_image: "http://via.placeholder.com/600x400",
+  thumb_image:"http://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 portfolio items created"
